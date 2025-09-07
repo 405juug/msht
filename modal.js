ea2378b5bb6d8090fdc1ddfa1b1cac96c6da4c90
wasd21xd <Avey21703@gmail.com>
2025-09-07 18:59:43 +0300

    add new logic card in services

--- modal.js
+++ modal.js
@@ -2,7 +2,6 @@
     const closeBtn = document.querySelector(".close-btn");
     const cancelBtn = document.querySelector(".btn-cancel");
 
-    // Открытие по клику на ссылку "Вход"
     document.querySelectorAll(".menu__link").forEach(link => {
     if (link.textContent.trim() === "Вход") {
     link.addEventListener("click", (e) => {
@@ -12,11 +11,9 @@
 }
 });
 
-    // Закрытие (крестик и Cancel)
     closeBtn.addEventListener("click", () => modal.style.display = "none");
     cancelBtn.addEventListener("click", () => modal.style.display = "none");
 
-    // Клик вне окна тоже закрывает
     window.addEventListener("click", (e) => {
     if (e.target === modal) {
     modal.style.display = "none";
